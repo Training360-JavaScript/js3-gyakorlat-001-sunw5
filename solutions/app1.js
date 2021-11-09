5fcba8b64de4c835736412fd7c28c7640fe715f0
Gabor Szasz <skywallalp@gmail.com>
2021-11-09 01:09:28 +0100

    content for test

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,13 @@
+const removeDuplicatesFromArray = (arrOriginal) => {
+  const uniqueArray = [];
 
+  arrOriginal.forEach((cv) => {
+    if (uniqueArray.some((v) => v === cv)) return;
+    uniqueArray.push(cv);
+  });
 
-
-
-
+  return uniqueArray;
+};
 
 // export default removeDuplicatesFromArray
+export { removeDuplicatesFromArray as default };
